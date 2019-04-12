e77c3edc5582617b9dfebba5434af2bcb31fed96
kingtamaaj <kingtamaaj@gmail.com>
2019-04-12 19:02:07 -0400

    Array update for top session

---
+++ Sprint-Challenge--JavaScript/challenges/objects-arrays.js
@@ -0,0 +1,162 @@
+$_$wp(1);
+let tyrannosaurus = ($_$w(1, 0), {
+    'name': 'tyrannosaurus',
+    'diet': 'carnivorous',
+    'weight': '7000kg',
+    'length': '12m',
+    'period': 'Late Cretaceious'
+});
+$_$w(1, 1), tyrannosaurus.speak = function () {
+    $_$wf(1);
+    return $_$w(1, 2), 'RAWERSRARARWERSARARARRRR!';
+};
+let stegosaurus = ($_$w(1, 3), {
+    'name': 'stegosaurus',
+    'diet': 'herbivorous',
+    'weight': '2000kg',
+    'length': '9m',
+    'period': 'Late Jurassic'
+});
+let velociraptor = ($_$w(1, 4), {
+    'name': 'velociraptor',
+    'diet': 'carnivorous',
+    'weight': '15kg',
+    'length': '8m',
+    'period': 'Late Cretaceious'
+});
+$_$w(1, 5), $_$tracer.log(tyrannosaurus.weight, 'tyrannosaurus.weight', 1, 5);
+$_$w(1, 6), $_$tracer.log(velociraptor.diet, 'velociraptor.diet', 1, 6);
+$_$w(1, 7), $_$tracer.log(stegosaurus.length, 'stegosaurus.length', 1, 7);
+$_$w(1, 8), $_$tracer.log(tyrannosaurus.period, 'tyrannosaurus.period', 1, 8);
+$_$w(1, 9), $_$tracer.log(tyrannosaurus.speak(), 'tyrannosaurus.speak()', 1, 9);
+const graduates = ($_$w(1, 10), [
+    {
+        'id': 1,
+        'first_name': 'Cynde',
+        'university': 'Missouri Southern State College',
+        'email': 'ctorry0@macromedia.com'
+    },
+    {
+        'id': 2,
+        'first_name': 'Saundra',
+        'university': 'The School of the Art Institute of Chicago',
+        'email': 'swhal1@state.gov'
+    },
+    {
+        'id': 3,
+        'first_name': 'Lambert',
+        'university': 'Marian College',
+        'email': 'lparham2@techcrunch.com'
+    },
+    {
+        'id': 4,
+        'first_name': 'Modestine',
+        'university': 'International Medical & Technological University',
+        'email': 'mdolder3@symantec.com'
+    },
+    {
+        'id': 5,
+        'first_name': 'Chick',
+        'university': 'Sultan Salahuddin Abdul Aziz Shah Polytechnic',
+        'email': 'camorts4@google.com.au'
+    },
+    {
+        'id': 6,
+        'first_name': 'Jakob',
+        'university': 'Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft',
+        'email': 'jharken5@spiegel.de'
+    },
+    {
+        'id': 7,
+        'first_name': 'Robbi',
+        'university': 'Salem University',
+        'email': 'rbrister6@redcross.org'
+    },
+    {
+        'id': 8,
+        'first_name': 'Colline',
+        'university': 'Coastal Carolina University',
+        'email': 'cbrosh7@alibaba.com'
+    },
+    {
+        'id': 9,
+        'first_name': 'Michail',
+        'university': 'Universidad Católica de Ávila',
+        'email': 'mrome8@shinystat.com'
+    },
+    {
+        'id': 10,
+        'first_name': 'Hube',
+        'university': 'Universitat Rovira I Virgili Tarragona',
+        'email': 'hlethbrig9@foxnews.com'
+    }
+]);
+$_$w(1, 11), zooAnimals = [
+    {
+        'animal_name': 'Jackal, asiatic',
+        'population': 5,
+        'scientific_name': 'Canis aureus',
+        'state': 'Kentucky'
+    },
+    {
+        'animal_name': 'Screamer, southern',
+        'population': 1,
+        'scientific_name': 'Chauna torquata',
+        'state': 'Alabama'
+    },
+    {
+        'animal_name': 'White spoonbill',
+        'population': 8,
+        'scientific_name': 'Platalea leucordia',
+        'state': 'Georgia'
+    },
+    {
+        'animal_name': 'White-cheeked pintail',
+        'population': 1,
+        'scientific_name': 'Anas bahamensis',
+        'state': 'Oregon'
+    },
+    {
+        'animal_name': 'Black-backed jackal',
+        'population': 2,
+        'scientific_name': 'Canis mesomelas',
+        'state': 'Washington'
+    },
+    {
+        'animal_name': 'Brolga crane',
+        'population': 9,
+        'scientific_name': 'Grus rubicundus',
+        'state': 'New Mexico'
+    },
+    {
+        'animal_name': 'Common melba finch',
+        'population': 5,
+        'scientific_name': 'Pytilia melba',
+        'state': 'Pennsylvania'
+    },
+    {
+        'animal_name': 'Pampa gray fox',
+        'population': 10,
+        'scientific_name': 'Pseudalopex gymnocercus',
+        'state': 'Connecticut'
+    },
+    {
+        'animal_name': 'Hawk-eagle, crowned',
+        'population': 10,
+        'scientific_name': 'Spizaetus coronatus',
+        'state': 'Florida'
+    },
+    {
+        'animal_name': 'Australian pelican',
+        'population': 5,
+        'scientific_name': 'Pelecanus conspicillatus',
+        'state': 'West Virginia'
+    }
+];
+const lowerCase = ($_$w(1, 12), []);
+$_$w(1, 13), $_$tracer.log(lowerCase, 'lowerCase', 1, 13);
+const largerPopulation = ($_$w(1, 14), []);
+$_$w(1, 15), $_$tracer.log(largerPopulation, 'largerPopulation', 1, 15);
+const populationTotal = ($_$w(1, 16), 0);
+$_$w(1, 17), $_$tracer.log(populationTotal, 'populationTotal', 1, 17);
+$_$wpe(1);
