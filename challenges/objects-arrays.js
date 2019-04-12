319a4fb4d380eb31f194662e13313e7fb151c499
kingtamaaj <kingtamaaj@gmail.com>
2019-04-12 15:16:16 -0400

    First objects commit

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,54 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+let tyrannosaurus = {
+  "name":"tyrannosaurus",
+  "diet": "carnivorous",
+  "weight": "7000kg",
+  "length": "12m",
+  "period": "Late Cretaceious"
+}
+  tyrannosaurus.speak = function() {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
-// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+  let stegosaurus = {
+    "name": "stegosaurus",
+    "diet": "herbivorous",
+    "weight": "2000kg",
+    "length":  "9m",
+    "period": "Late Jurassic"
+  }
+  
+  // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+   let velociraptor = {
+      "name": "velociraptor",
+      "diet": "carnivorous",
+      "weight": "15kg",
+      "length": "8m",
+      "period": "Late Cretaceious",
+    }
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.speak());
 
 
 // ==== Arrays ====
@@ -46,11 +72,17 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
-
-Once you have the new array created, sort the universities alphabetically and log the result. */
+// Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 const universities = [];
-console.log(universities)
+for(let i = 0; 1 < universities.length; i++); {
+  function getSchools(){
+    console.log(`${graduates[i].universities}`);
+  }
+}
+
+// Once you have the new array created, sort the universities alphabetically and log the result. 
+// const universities = []
+// console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -88,7 +120,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
+const animalNames = [ ];
 console.log(animalNames);
 
 /* Request 2: .map()    
